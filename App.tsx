393e3fa35933bca078e05bf95614802cd2c755b1
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-19 20:09:03 -0300

    Feat: adicionando theme da aplicação

--- App.tsx
+++ App.tsx
@@ -1,24 +1,20 @@
-import { View, Text } from "react-native";
-
-
-import { useFonts, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
-
-import { Product } from "./src/screens/Products";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium
-  })
-
-  if(!fontsLoaded){
-    return <Text> Carregando ..</Text>
-  }
-
   return (
-    <View >
-      {fontsLoaded ? <Product /> : <Text> Carregando ...</Text>}
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
