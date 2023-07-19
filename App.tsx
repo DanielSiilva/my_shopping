1c2b1cf8f7a3d667ff5a3c611f8347ca42d45a00
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-19 00:21:44 -0300

    Feat: iniciando pagina de product

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+
+
+import { useFonts, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+
+import { Product } from "./src/screens/Products";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium
+  })
+
+  if(!fontsLoaded){
+    return <Text> Carregando ..</Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      {fontsLoaded ? <Product /> : <Text> Carregando ...</Text>}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Products/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text } from "react-native";
+
+import {
+  Container
+} from './styles'
+
+export function Product (){
+
+  return (
+    <Container >
+      <Text> Iniciando aplicação</Text>
+    </Container>
+  )
+  
+}
