e11c35e105167aaf03bc2d4bba81b6889d9488b8
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-19 20:24:56 -0300

    Feat: configurando fonts da aplicação

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text } from 'react-native';
+
+import {ThemeProvider} from 'styled-components/native'
+import theme from './src/theme/index';
+import {useFonts, Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium
+  })
+
+  if(!fontsLoaded){
+    return <Text> Carregando ...</Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme} >
+      <>
+        <StatusBar
+          backgroundColor='transparent'
+          translucent
+        />
+      </>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
