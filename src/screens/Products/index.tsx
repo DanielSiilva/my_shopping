ebd10df547e8f5bf70d06b85b6417f89612571fd
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-20 08:53:53 -0300

    Feat: componente FormBox

--- src/screens/Products/index.tsx
+++ src/screens/Products/index.tsx
@@ -4,11 +4,13 @@ import {
   Container 
 } from "./styles";
 import { Header } from "../../components/Header";
+import { FormBox } from "../../components/FormBox/FormBox";
 
 export function Products(){
   return (
     <Container>
       <Header title="Lista de compras"/>
+      <FormBox />
     </Container>
   )
 }
