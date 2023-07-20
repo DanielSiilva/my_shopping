ec30afdf596da25bdf0e1690f011bde5dbd28d2c
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-19 22:14:57 -0300

    Feat: adicionando componente  de loading

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import {Text } from 'react-native';
+import {Text, StatusBar } from 'react-native';
 
 import {ThemeProvider} from 'styled-components/native'
 import theme from './src/theme/index';
+
 import {useFonts, Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,17 +12,17 @@ export default function App() {
     Roboto_500Medium
   })
 
-  if(!fontsLoaded){
-    return <Text> Carregando ...</Text>
-  }
+
 
   return (
     <ThemeProvider theme={theme} >
       <>
         <StatusBar
-          backgroundColor='transparent'
+          barStyle="dark-content"
+          backgroundColor="transparent"
           translucent
         />
+        {fontsLoaded ? <Text>Carregando ...</Text> : <Loading />}
       </>
     </ThemeProvider>
   );
