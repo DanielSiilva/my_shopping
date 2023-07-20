81729ef528e579e73440badd24e963ed1b7f2a3a
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-20 11:18:44 -0300

    Feat: adicionando shoppingList

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,9 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
+import {FlatList} from 'react-native'
+import {styles} from './styles'
+
+import {Product, ProductPros} from '../Product'
+
+import {shoppingListExample} from '../../utils/shopping.list.data'
 
 export function ShoppingList(){
+  const [products, setProducts] = useState<ProductPros[]>(shoppingListExample)
 
   return (
-    <>
-    </>
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={({item}) => <Product data={item} />}
+      showsVerticalScrollIndicator={false}
+      style={styles.list}
+      contentContainerStyle={styles.content}
+    />
+   
   )
 }
--- src/components/ShoppingList/styles.ts
+++ src/components/ShoppingList/styles.ts
@@ -1,1 +1,11 @@
-import styled from "styled-components/native";
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  list: {
+    width: '100%',
+    padding: 24,
+  },
+  content: {
+    paddingBottom: 100
+  }
+});
