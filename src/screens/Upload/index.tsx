b1d4f18bb2f6fa535ea88f57d66824fc1418d139
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-21 22:37:23 -0300

    Feat: screens Upload

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,4 +1,5 @@
-import {Text} from 'react-native'
+import React, {useState} from 'react'
+import * as ImagePicker from 'expo-image-picker'
 
 import {
   Container,
@@ -6,11 +7,53 @@ import {
   Progress,
   Transferred
 } from './styles'
+import { Header } from '../../components/Header'
+import { Photo } from '../../components/Photo'
+import { Button } from '../../components/Button'
 
 export function Upload(){
+  const [image, setImage] = useState('')
+
+  async function handlePickImage() {
+    const {status} = await ImagePicker.requestCameraPermissionsAsync()
+
+    if(status == 'granted'){
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4,4],
+        quality:1, 
+      });
+
+      if(!result.canceled){
+        setImage(result.assets[0].uri)
+      }
+    }
+  }
+
   return (
-    <>
-      <Text> Page Upload</Text>
-    </>
+    <Container>
+      <Header title='Lista de compras' />
+
+      <Content>
+        <Photo 
+          uri={image}
+          onPress={handlePickImage}
+        />
+
+        <Button
+          title='Fazer upload'
+          onPress={() => {}}
+        />
+
+        <Progress>
+          0%
+        </Progress>
+
+        <Transferred>
+          0 de 100 bytes transferido
+        </Transferred>
+      </Content>
+      
+    </Container>
   )
 }
