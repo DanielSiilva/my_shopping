fd7a48be56c9ddbd79c794f998d3e5dccd834882
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-21 21:12:20 -0300

    Feat: estrutura da pagina de Receipts

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,9 +1,35 @@
-import {Text} from 'react-native'
+import React from "react"
+import { FlatList} from 'react-native'
+
+import { Header } from "../../components/Header"
+
+import {
+  Container,
+  PhotoInfo
+} from './styles'
+
+import { photosData } from '../../utils/photo.data';
 
 export function Receipts(){
   return (
-    <>
-      <Text> Page Receipts</Text>
-    </>
+    <Container>
+      <Header title="Comprovantes" />
+
+      <PhotoInfo>
+        Informações da foto
+      </PhotoInfo>
+
+      <FlatList
+        data={photosData}
+        keyExtractor={item => item.name}
+        renderItem={({item}) => (
+          
+        )}
+        contentContainerStyle={{paddingBottom: 100}}
+        showsVerticalScrollIndicator={false}
+        style={{width: '100%', padding: 24}}
+      
+      />
+    </Container>
   )
 }
