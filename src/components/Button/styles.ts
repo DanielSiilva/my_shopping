40d49c6ac8040623d46ee280d3752446be17ecb0
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-21 22:07:19 -0300

    Feat: componente Button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,19 @@
+import { TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  height: 60px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color:${({ theme }) => theme.COLORS.GREEN};
+  border-radius: 5px;
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.FONTS.REGULAR};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  margin-left: 7px;
+`;
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import {Text} from 'react-native'
+
+import { Input } from '../../components/Input'
+// import { Button } from '../../components/'
+
+import {
+  Account,
+  Container,
+  Title,
+  Subtitle
+} from './styles'
+
 
 export function SignIn(){
   return (
-    <>
-      <Text> Page SignIn</Text>
-    </>
+    <Container>
+     <Title>MyShopping</Title>
+     <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
+
+     <Input
+      placeholder='e-mail'
+      keyboardType='email-address'
+     />
+
+    <Input
+      placeholder='senha'
+      secureTextEntry
+     />
+
+     
+    </Container>
   )
 }
